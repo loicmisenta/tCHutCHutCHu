04533e725ea7d1eb5afba433d692df798dc9156a
Alex <alexandra.lagutova@epfl.ch>
2021-02-22 17:16:51 +0100

    test

--- src/ch/epfl/tchu/game/Card.java
+++ src/ch/epfl/tchu/game/Card.java
@@ -1,18 +1,28 @@
 package ch.epfl.tchu.game;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public enum Card {
-    BLACK("wagon noir"), VIOLET("wagon violet"), BLUE("wagon bleu"), GREEN("wagon vert"),
-    YELLOW("wagon jaune"), ORANGE("wagon orange"), RED("wagon rouge"), WHITE("wagon blanc"),
-    LOCOMOTIVE("locomotive");
+    BLACK("wagon noir", Color.BLACK), VIOLET("wagon violet", Color.VIOLET), BLUE("wagon bleu", Color.BLUE),
+    GREEN("wagon vert", Color.GREEN), YELLOW("wagon jaune", Color.YELLOW), ORANGE("wagon orange", Color.ORANGE),
+    RED("wagon rouge", Color.RED), WHITE("wagon blanc", Color.RED),
+    LOCOMOTIVE("locomotive", null);
+
     private String value;
-    private Card (String value){
+    private Color color;
+    private Card (String value, Color color){
         this.value = value;
+        this.color = color;
     }
     public static final List<Card> ALL = List.of(Card.values());
     public static final int COUNT = ALL.size();
-    //public static final List<Card> CARS = new ArrayList<Card>(BLACK , VIOLET, BLUE, GREEN, YELLOW, ORANGE, RED, WHITE);
+    public static final List<Card> CARS = new ArrayList<Card>(Collections.singleton(ALL.remove(8)));
+    public static Card of(Color color){
+        return valueOf(color.name());
+    }
 
-}
+public static void main (String[] arg){
+    System.out.print(of(Color.WHITE));
+        }}
