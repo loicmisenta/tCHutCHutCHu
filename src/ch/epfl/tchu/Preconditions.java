0434d852e6f9e2c214c1a77e44b07c37e88605e0
Alex <alexandra.lagutova@epfl.ch>
2021-02-22 15:55:57 +0100

    test

--- src/ch/epfl/tchu/Preconditions.java
+++ src/ch/epfl/tchu/Preconditions.java
@@ -1,4 +1,10 @@
 package ch.epfl.tchu;
 
-public class Preconditions {
+public final class Preconditions {
+    private Preconditions() {}
+    private static void checkArgument(boolean b){
+            if (!b){
+                throw new IllegalArgumentException();
+        }
+    }
 }
--- src/ch/epfl/tchu/game/Color.java
+++ src/ch/epfl/tchu/game/Color.java
@@ -1,5 +1,10 @@
 package ch.epfl.tchu.game;
 
+//import java.util.List;
+
 public enum Color {
-    BLACK, VIOLET, BLUE, GREEN, YELLOW, ORANGE , RED , WHITE;
+    BLACK , VIOLET, BLUE, GREEN, YELLOW, ORANGE , RED , WHITE;
 }
+    //List<Color> ALL = (List<Color>) Color.values();
+
+    //int COUNT;
