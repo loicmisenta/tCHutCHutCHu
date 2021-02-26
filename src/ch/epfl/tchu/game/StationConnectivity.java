0ef358e55bce2c7846f36c115be181f6a6e91070
Loïc Misenta <misentaloic@gmail.com>
2021-02-26 11:24:54 +0100

    trip

---
+++ src/ch/epfl/tchu/game/StationConnectivity.java
@@ -0,0 +1,5 @@
+package ch.epfl.tchu.game;
+
+public interface StationConnectivity {
+    public abstract boolean connected(Station s1, Station s2);
+}
---
+++ src/ch/epfl/tchu/game/Trip.java
@@ -0,0 +1,60 @@
+package ch.epfl.tchu.game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public final class Trip {
+
+    private final Station from;
+    private final Station to;
+    private final int points;
+
+    public Trip(Station from, Station to, int points) {
+        if(from == null || to == null){
+            throw new NullPointerException("Une des deux gares est nulle");
+        }
+        if (points <= 0){
+            throw new IllegalArgumentException("Points négatifs");
+        }
+        this.from = Objects.requireNonNull(from);
+        this.to = Objects.requireNonNull(to);
+        this.points = points;
+    }
+    public static List<Trip> all(List<Station> from, List<Station> to, int points){
+        if ((from.isEmpty() || to.isEmpty()) || points<=0){
+            throw new IllegalArgumentException("Liste vide ou points négatifs");
+        }
+
+        List<Trip> trajet = new ArrayList<>();
+        for (Station f:
+             from) {
+            for (Station t:
+                 to) {
+                trajet.add(new Trip(f, t, points));
+            }
+        }
+        return trajet;
+    }
+
+    public Station from() {
+        return from;
+    }
+
+    public Station to() {
+        return to;
+    }
+
+    public int points() {
+        return points;
+    }
+    public int points(StationConnectivity connectivity){
+        if(connectivity.connected(from(), to())){
+            return points();
+        }
+        else{
+            return -1*points();
+        }
+
+    }
+}
