8eef989724ad6475dbe7cf0d2345827866bd96a4
Loïc Misenta <misentaloic@gmail.com>
2021-03-04 15:52:57 +0100

    toujours route

--- src/ch/epfl/tchu/game/Trail.java
+++ src/ch/epfl/tchu/game/Trail.java
@@ -1,5 +1,7 @@
 package ch.epfl.tchu.game;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public final class Trail {
@@ -8,18 +10,68 @@ public final class Trail {
     public Station station2;
 
     //faire un constructeur qui n'est pas forcément public
-    private Trail (List<Route> routes){
-
+    public Trail (List<Route> routes){
+        this.station1 = routes.get(0).station1();
+        this.station2 = routes.get(routes.size()-1).station2();
+        for (Route r: routes) {
+            this.length += r.length();
+        }
     }
+
+    public int length() {
+        return length;
+    }
+
+    public Station station1() {
+        return station1;
+    }
+
+    public Station station2() {
+        return station2;
+    }
+
+
     /**
      *
      * @param routes
      */
-    static void longest(List<Route> routes){
+    static Trail longest(List<Route> routes){
 
-        //FAUX ::::
-        List<Route> cs = List.copyOf(routes);  //il faudra faire une copie
-        List<Route> rs = List.copyOf(routes);  // à chaque fois
+
+        List<Route> cs = new ArrayList<>(routes);
+        List<Route> lastcs = new ArrayList<>();
+
+
+
+        while(!cs.isEmpty()){
+            List<Route> csPrime = new ArrayList<>();
+            for (Route c: cs) {
+                List<Route> rs = new ArrayList<>(routes);
+
+
+                rs.removeAll(Collections.singleton(c));//PEUT ETRE IF ON SAIS PAS TROP KOI
+
+
+
+                for (Route r: rs) {
+                    if(!r.station1().equals(c.station2())){
+                        rs.remove(r);
+                    }
+
+                    else{
+                        Trail t = new Trail(List.of(r, c));
+                        csPrime.addAll(List.of(c, r));
+                    }
+                }
+                if(csPrime.isEmpty()){
+                    lastcs = cs;
+                }
+                cs = csPrime;
+
+            }
+        }
+
+        return new Trail(lastcs);
         //liste de tous les chemins constitués d'une seule route
 
     }
---
+++ test/ch/epfl/tchu/game/TrailTest.java
@@ -0,0 +1,18 @@
+package ch.epfl.tchu.game;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrailTest {
+
+    @Test
+    void TestlongestWithTwoRoutes(){
+        var listRouteTest = List.of(ChMap.routes().get(2), ChMap.routes().get(5));
+        var expectedValue = 4;
+        assertEquals(expectedValue, Trail.longest(listRouteTest).length());
+    }
+
+}
