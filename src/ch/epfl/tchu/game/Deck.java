aabf99eef4322d884f342be1c6ae6c5b069b531e
Alex <alexandra.lagutova@epfl.ch>
2021-03-05 18:52:03 +0100

    Deck

--- src/ch/epfl/tchu/game/Deck.java
+++ src/ch/epfl/tchu/game/Deck.java
@@ -1,8 +1,30 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.SortedBag;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 /**
  * @author loicmisenta
  * @author lagutovaalexandra
+ * représentant un tas de cartes
  */
-public class Deck {
+public final class Deck<C extends Comparable<C>>  {
+    private final C c;
+    private Deck(C c){
+
+    }
+
+    /**
+     * Une méthode qui mélange:
+     * @param cards le tas de cartes
+     * @param rng générateur de nombres aléatoires
+     * @param <C> le type des cartes du tas
+     * @return
+     */
+    <C extends Comparable<C>> Deck<C> of(SortedBag<C> cards, Random rng){
+        return new Deck<C>(Collections.shuffle((cards), rng));
+    }
 }
