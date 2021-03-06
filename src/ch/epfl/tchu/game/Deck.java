fbb224aa6172664a33dedd666a4def724ed48bfd
Alex <alexandra.lagutova@epfl.ch>
2021-03-06 10:33:20 +0100

    Deck

--- src/ch/epfl/tchu/game/Deck.java
+++ src/ch/epfl/tchu/game/Deck.java
@@ -1,10 +1,13 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+
+import static java.util.List.copyOf;
 
 /**
  * @author loicmisenta
@@ -12,10 +15,15 @@ import java.util.Random;
  * représentant un tas de cartes
  */
 public final class Deck<C extends Comparable<C>>  {
-    private final C c;
-    private Deck(C c){
 
+    private final C c;
+    private SortedBag<C> cards;
+
+    private Deck(C c, SortedBag<C> cards){
+        this.c  = c;
+        this.cards = cards;
     }
+
 
     /**
      * Une méthode qui mélange:
@@ -27,4 +35,37 @@ public final class Deck<C extends Comparable<C>>  {
     <C extends Comparable<C>> Deck<C> of(SortedBag<C> cards, Random rng){
         return new Deck<C>(Collections.shuffle(cards.toList(), rng));
     }
+
+    private SortedBag<C> getCards(){
+        return cards;
+    }
+
+    public int size(){
+        return getCards().size();
+    }
+
+    public boolean isEmpty(){
+        return getCards().isEmpty();
+    }
+
+    public C topCard(){
+        Preconditions.checkArgument(!isEmpty());
+        return getCards().get(0);
+    }
+
+    public Deck<C> withoutTopCard(){
+        Preconditions.checkArgument(!isEmpty());
+        return new Deck<C>(c , SortedBag.of((cards.toList()).subList(1, size())));
+    }
+
+    public SortedBag<C> topCards(int count){
+        Preconditions.checkArgument((count >= 0 ) && (count <= size()));
+        return SortedBag.of((cards.toList()).subList(1, count + 1));
+    }
+
+    public Deck<C> withoutTopCards(int count){
+        Preconditions.checkArgument((count >= 0 ) && (count <= size()));
+        return new Deck<C>(c ,SortedBag.of((cards.toList()).subList(count + 1, size())));
+    }
+
 }
