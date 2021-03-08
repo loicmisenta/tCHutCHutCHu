e2e98ebbdf3e17877e5ad098c3ec5693d815f186
Loïc Misenta <misentaloic@gmail.com>
2021-03-08 13:40:17 +0100

    JE COMPRENDS PAS...

--- src/ch/epfl/tchu/gui/Info.java
+++ src/ch/epfl/tchu/gui/Info.java
@@ -1,9 +1,21 @@
 package ch.epfl.tchu.gui;
 
+import ch.epfl.tchu.game.Card;
+
 /**
  * @author loicmisenta
  * @author lagutovaalexandra
  */
 public final class Info {
+    private String playerName;
 
+    public Info(String playerName){
+        this.playerName = playerName;
+    }
+
+    public static String cardName(Card card, int count){
+        switch (card){
+            case StringsFr
+        }
+    }
 }
