c668643a8d9390805c63d5ca1221ff3f11e0379d
Alex <alexandra.lagutova@epfl.ch>
2021-03-24 15:04:42 +0100

    Erreurs dans GameState On arrive même pas à créer un PublicGameState

--- test/ch/epfl/tchu/game/GameStateTest.java
+++ test/ch/epfl/tchu/game/GameStateTest.java
@@ -19,13 +19,14 @@ public class GameStateTest {
 
     CardState etatDesCartes = CardState.of(Deck.of(SortedBag.of(3, Card.BLUE, 2, Card.BLACK), new Random()));
 
-    //TODO creation de Map
+    PlayerState etatJoueur = new PlayerState(SortedBag.of(
+            1, new Ticket(new Station(1, "Bâle"), new Station(3, "Berne"), 5),
+            1, new Ticket(new Station(1, "Bâle"), new Station(4, "Brigue"), 10)),
+            SortedBag.of(3, Card.WHITE, 4, Card.LOCOMOTIVE),
+            List.of(new Route("BAL_DEL_1", new Station(1, "Bâle"), new Station(8, "Delémont"), 2, Route.Level.UNDERGROUND, Color.YELLOW)));
+
     Map<PlayerId, PlayerState> map = Map.of(
-            PlayerId.PLAYER_1 , new PlayerState(SortedBag.of(
-                    1, new Ticket(new Station(1, "Bâle"), new Station(3, "Berne"), 5),
-                    1, new Ticket(new Station(1, "Bâle"), new Station(4, "Brigue"), 10)),
-                    SortedBag.of(3, Card.WHITE, 4, Card.LOCOMOTIVE),
-                    List.of(new Route("BAL_DEL_1", new Station(1, "Bâle"), new Station(8, "Delémont"), 2, Route.Level.UNDERGROUND, Color.YELLOW))));
+            PlayerId.PLAYER_1 ,etatJoueur, PlayerId.PLAYER_2, etatJoueur );
     GameState gameState = new GameState(40, etatDesCartes, PlayerId.PLAYER_1, map, PlayerId.PLAYER_2, Deck.of(
             SortedBag.of(2, new Ticket(new Station(1, "Bâle"), new Station(3, "Berne"), 5),
                          1, new Ticket(new Station(33, "Zürich"), new Station(17, "Lugano"), 9)), new Random()));
@@ -66,19 +67,21 @@ public class GameStateTest {
 
     @Test
     void lastTurnBeguins(){
+        PlayerState etatJoueur1 = new PlayerState(SortedBag.of(
+                1, new Ticket(new Station(1, "Bâle"), new Station(3, "Berne"), 5),
+                1, new Ticket(new Station(1, "Bâle"), new Station(4, "Brigue"), 10)),
+                SortedBag.of(3, Card.WHITE, 4, Card.LOCOMOTIVE),
+                List.of(new Route("BAL_DEL_1", new Station(1, "Bâle"), new Station(8, "Delémont"), 2, Route.Level.UNDERGROUND, Color.YELLOW),
+                        new Route("BAL_DEL_1", new Station(1, "Bâle"), new Station(8, "Delémont"), 2, Route.Level.UNDERGROUND, Color.YELLOW),
+                        new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
+                        new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
+                        new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
+                        new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
+                        new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
+                        new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null)));
+
         Map<PlayerId, PlayerState> maplast = Map.of(
-                PlayerId.PLAYER_1 , new PlayerState(SortedBag.of(
-                        1, new Ticket(new Station(1, "Bâle"), new Station(3, "Berne"), 5),
-                        1, new Ticket(new Station(1, "Bâle"), new Station(4, "Brigue"), 10)),
-                        SortedBag.of(3, Card.WHITE, 4, Card.LOCOMOTIVE),
-                        List.of(new Route("BAL_DEL_1", new Station(1, "Bâle"), new Station(8, "Delémont"), 2, Route.Level.UNDERGROUND, Color.YELLOW),
-                                new Route("BAL_DEL_1", new Station(1, "Bâle"), new Station(8, "Delémont"), 2, Route.Level.UNDERGROUND, Color.YELLOW),
-                                new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
-                                new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
-                                new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
-                                new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
-                                new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null),
-                                new Route("GEN_YVE_1", new Station(10, "Genève"), new Station(31, "Yverdon"), 6, Route.Level.OVERGROUND, null))));
+                PlayerId.PLAYER_1 , etatJoueur1, PlayerId.PLAYER_2, etatJoueur );
         GameState lastTurnGameState = new GameState(10, etatDesCartes, PlayerId.PLAYER_1, maplast, null, Deck.of(SortedBag.of(new Ticket(new Station(1, "Bâle"), new Station(3, "Berne"), 5)), new Random()));
         Assertions.assertTrue(lastTurnGameState.lastTurnBegins());
         assertFalse(gameState.lastTurnBegins());
