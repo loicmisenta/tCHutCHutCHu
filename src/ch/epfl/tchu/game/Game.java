fdaa81c7efba3f334cb44849f44dd9938bec4c41
Alex <alexandra.lagutova@epfl.ch>
2021-03-26 14:03:48 +0100

    ...

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -1,5 +1,6 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
 
 import java.util.Map;
@@ -10,7 +11,16 @@ import java.util.Random;
  * @author lagutovaalexandra
  */
 public class Game {
+
+    /**
+     * Fait jouer une partie
+     * @param players joueurs
+     * @param playerNames les noms des joueurs
+     * @param tickets les billets dissponibles
+     * @param rng générateur aléatoire de nombres
+     */
     public void play(Map<PlayerId, Player> players, Map<PlayerId, String> playerNames, SortedBag<Ticket> tickets, Random rng){
+        Preconditions.checkArgument((players.size() == 2 )|| (playerNames.size() == 2));
 
     }
 }
