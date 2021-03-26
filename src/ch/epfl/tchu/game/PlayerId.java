2f148b373a60baedd076866953f4d00d12ef3899
Loïc Misenta <misentaloic@gmail.com>
2021-03-26 12:48:49 +0100

    TestGStatecommente

--- src/ch/epfl/tchu/game/PlayerId.java
+++ src/ch/epfl/tchu/game/PlayerId.java
@@ -17,7 +17,6 @@ public enum PlayerId {
     /**
      * Méthode qui retourne 'identité du joueur qui suit celui auquel on l'applique
      */
-    //PLUS FACILE???
     public PlayerId next(){ if(this == PLAYER_1){ return PLAYER_2;}
     else { return PLAYER_1; } }
 }
