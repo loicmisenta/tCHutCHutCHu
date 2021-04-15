9ed352d8e4a70299d9f85352aee0d3dbd6bed46d
Loïc Misenta <misentaloic@gmail.com>
2021-04-15 14:12:40 +0200

    Clean

--- src/ch/epfl/tchu/Preconditions.java
+++ src/ch/epfl/tchu/Preconditions.java
@@ -5,10 +5,11 @@ package ch.epfl.tchu;
  * @author loicmisenta
  * @author lagutovaalexandra
  *
- Cette méthode contrôle que l'appel de la méthode ne se fait
- qu'avec des arguments valides.
- @throws IllegalArgumentException  // TODO WHY NOT ALLOWED IN HERE ?
+ * Cette méthode contrôle que l'appel de la méthode ne se fait
+ * qu'avec des arguments valides.
+ * @throws IllegalArgumentException
  */
+
 public final class Preconditions {
     private Preconditions() {}
     public static void checkArgument(boolean b){
