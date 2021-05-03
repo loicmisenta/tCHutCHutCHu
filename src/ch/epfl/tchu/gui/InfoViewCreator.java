f6499ec4d3e52599c72bdcf683ed8c145d841588
Loïc Misenta <misentaloic@gmail.com>
2021-05-03 17:31:36 +0200

    ETAPE 9

--- src/ch/epfl/tchu/gui/InfoViewCreator.java
+++ src/ch/epfl/tchu/gui/InfoViewCreator.java
@@ -3,6 +3,7 @@ package ch.epfl.tchu.gui;
 import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.game.PlayerId;
 import javafx.beans.binding.Bindings;
+import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.VBox;
@@ -28,34 +29,17 @@ public class InfoViewCreator {
      * @param text
      * @return
      */
+    //TODO pourquoi on utilise pas playerId
     public static Node createInfoView(PlayerId playerId, Map<PlayerId, String> playerIdStringMap, ObservableGameState obsGS, List<Text> text){
         VBox vbox = new VBox();
         vbox.getStylesheets().addAll("info.css", "colors.css");
-
-        Separator separator = new Separator();//TODO autre chose?
-        vbox.getChildren().add(separator);
-
-        //message
-        TextFlow textFlow = new TextFlow();
-        textFlow.setId("game-info");
-        vbox.getChildren().add(textFlow);
-
-        /*
-        //TODO afficher 4 messages maxà chaque fois???????
-        for (Text string: text) {
-            Text textMessage = new Text(string);
-            textFlow.getChildren().addAll(textMessage);
-        }
-        //TODO ne possede aucun fils?
-
-
-         */
 
         VBox vboxPlayerStats = new VBox();
         vboxPlayerStats.setId("player-stats");
         vbox.getChildren().add(vboxPlayerStats);
 
         //statistique joueur
+
         for (PlayerId id: PlayerId.values()) {
             TextFlow statistiquesJoueur = new TextFlow();
             statistiquesJoueur.setId(String.valueOf(id));
@@ -66,6 +50,20 @@ public class InfoViewCreator {
             Text text1 = new Text();
             text1.textProperty().bind(Bindings.format(StringsFr.PLAYER_STATS, playerIdStringMap.get(id) ,obsGS.ownedTicketsReadOnly(id), obsGS.ownedCardReadOnly(id), obsGS.ownedCarsReadOnly(id), obsGS.ownedConstructPointsReadOnly(id))); //TODO doit le bind à une Valeur observée !!!
             statistiquesJoueur.getChildren().addAll(circle, text1);
+        }
+        Separator separator = new Separator(Orientation.HORIZONTAL);//TODO autre chose?
+        vbox.getChildren().add(separator);
+
+
+        //message
+        TextFlow textFlow = new TextFlow();
+        textFlow.setId("game-info");
+        vbox.getChildren().add(textFlow);
+        List <Text> listText = text.size() > 5? text.subList(text.size()-6, text.size()-2) : text;
+
+        for (Text string: listText) {
+            Text textMessage = new Text(string.getText());
+            textFlow.getChildren().addAll(textMessage);
         }
 
         return vbox;
