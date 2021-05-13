3290acb72b17c7059d838b22304d4b8da2117e64
Alex <alexandra.lagutova@epfl.ch>
2021-05-13 09:24:03 +0200

    Etape 11 suite

--- src/ch/epfl/tchu/gui/ServerMain.java
+++ src/ch/epfl/tchu/gui/ServerMain.java
@@ -1,14 +1,22 @@
 package ch.epfl.tchu.gui;
 
+import ch.epfl.tchu.game.Player;
 import ch.epfl.tchu.net.RemotePlayerProxy;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         //TODO il faut prendre le m Socket
-        //RemotePlayerProxy playerProxy = new RemotePlayerProxy();
+        try (ServerSocket serverSocket = new ServerSocket(5108);
+             Socket socket = serverSocket.accept()) {
+            Player playerProxy = new RemotePlayerProxy(socket);
+        }
+
     }
 }
