80e6f4dc36bc5048800df632496802b7a9e81dd4
Alex <alexandra.lagutova@epfl.ch>
2021-05-14 17:25:48 +0200

    Etape 11 suite

--- src/ch/epfl/tchu/gui/ClientMain.java
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -1,19 +1,26 @@
 package ch.epfl.tchu.gui;
 
 import ch.epfl.tchu.net.RemotePlayerClient;
+import ch.epfl.tchu.net.RemotePlayerProxy;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.stage.Stage;
 
+import java.util.List;
+
 public class ClientMain extends Application {
 
+    public static void main(String[] args) { launch(args);}
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         GraphicalPlayerAdapter graphicalPlayerAdapter = new GraphicalPlayerAdapter();
-        RemotePlayerClient playerClient = new RemotePlayerClient(graphicalPlayerAdapter, "localhost", 5108);
+        List<String> list = this.getParameters().getRaw();
+        RemotePlayerClient playerClient = new RemotePlayerClient(graphicalPlayerAdapter, list.get(0), );
         playerClient.run();
 
-        //TODO quoi faire avec getParameters et getRaw
+
+        System.out.println(list);
+
     }
 }
--- src/ch/epfl/tchu/gui/ServerMain.java
+++ src/ch/epfl/tchu/gui/ServerMain.java
@@ -7,15 +7,20 @@ import javafx.stage.Stage;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class ServerMain extends Application {
+
+    public static void main(String[] args) { launch(args);}
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         //TODO il faut prendre le m Socket
         try (ServerSocket serverSocket = new ServerSocket(5108);
              Socket socket = serverSocket.accept()) {
-            Player playerProxy = new RemotePlayerProxy(socket);
+             Player playerProxy = new RemotePlayerProxy(socket);
+
+
         }
 
     }
