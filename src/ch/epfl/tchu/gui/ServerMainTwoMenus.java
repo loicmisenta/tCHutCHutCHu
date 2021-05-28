dd8e63de7a2480b470299541a41aadd0caa0bb70
Alex <alexandra.lagutova@epfl.ch>
2021-05-28 13:37:56 +0200

    ETAPE 12

--- src/ch/epfl/tchu/gui/ServerMainTwoMenus.java
+++ src/ch/epfl/tchu/gui/ServerMainTwoMenus.java
@@ -7,6 +7,7 @@ import ch.epfl.tchu.game.Player;
 import ch.epfl.tchu.game.PlayerId;
 import ch.epfl.tchu.net.RemotePlayerProxy;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -40,14 +41,15 @@ public final class ServerMainTwoMenus extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Platform.setImplicitExit(false);
         IntegerProperty s = ChooseNbPlayersCreator.ChooseNbPlayers(primaryStage);
         s.addListener((o, oV, nV)-> {
             System.out.println(nV.toString());
             nbJoueurs.set(nV.intValue());
             if (!(nV.intValue() == 0)){
                 try {
-                    startGame();
-                    //startMenu(primaryStage);
+                    //startGame();
+                    startMenu(primaryStage);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -56,6 +58,7 @@ public final class ServerMainTwoMenus extends Application {
     }
 
     public void startMenu(Stage primaryStage) {
+        Platform.setImplicitExit(false);
         StringProperty s = MenuViewCreator.createMenuView(primaryStage);
         s.addListener((o, oV, nV)-> new Thread( () ->{
             joueur.set(nV);
@@ -80,8 +83,8 @@ public final class ServerMainTwoMenus extends Application {
             Map<PlayerId, String> map = new EnumMap<>(PlayerId.class);
             int i = 1;
             mapPlayer.put(PlayerId.PLAYER_1, new GraphicalPlayerAdapter());
-            //map.put(PlayerId.PLAYER_1, joueur.getValue());
-            map.put(PlayerId.PLAYER_1, arguments.get(0));
+            map.put(PlayerId.PLAYER_1, joueur.getValue());
+            //map.put(PlayerId.PLAYER_1, arguments.get(0));
             System.out.println(nbJoueurs.getValue());
             for (PlayerId id: PlayerId.ALL.subList(0, nbJoueurs.getValue())) {
                 if(id == PlayerId.PLAYER_1) continue;
@@ -89,6 +92,7 @@ public final class ServerMainTwoMenus extends Application {
                 Socket socket = serverSocket.accept();
                 mapPlayer.put(id, new RemotePlayerProxy(socket));
             }
+            Platform.setImplicitExit(true);
             new Thread(() -> Game.play(mapPlayer, map, SortedBag.of(ChMap.tickets()), new Random())).start();
 
         }
